28e3de0f5f5969e06a8a0769674f003f1d15d4a1
joseph.lauth <joseph.lauth@CPX-5UCKMCA0IMV.dir.svc.accenture.com>
2019-10-02 16:53:35 -0500

    1st commit

---
+++ WebDriverDemo/src/main/java/com/pluralsight/WebDriverDemo.java
@@ -0,0 +1,28 @@
+package com.pluralsight;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverDemo {
+
+	public static void main(String[] args) {
+			
+		//WebDriver driver = new FirefoxDriver();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+
+		WebElement searchField = driver.findElement(By.cssSelector(".gLFyf"));
+		searchField.sendKeys("pluralsight");
+		searchField.submit();
+		
+/*		WebElement imagesLink = driver.findElements(By.linkText("Images")).get(0);
+		imagesLink.click();
+		
+		WebElement imageElement = driver.findElements(By.cssSelector("a")).get(0);
+		WebElement imageLink = imageElement.findElements(By.tagName("img")).get(0);
+		imageLink.click();
+*/	
+	}
+}
