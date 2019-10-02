8a0dccfcfbcfbf7db5e7c6492bb700c68a303369
joseph.lauth <joseph.lauth@CPX-5UCKMCA0IMV.dir.svc.accenture.com>
2019-10-02 17:52:50 -0500

    commit

--- WebDriverDemo/src/main/java/com/pluralsight/WebDriverDemo.java
+++ WebDriverDemo/src/main/java/com/pluralsight/WebDriverDemo.java
@@ -18,12 +18,12 @@ public class WebDriverDemo {
 		searchField.sendKeys("pluralsight");
 		searchField.submit();
 		
-/*		WebElement imagesLink = driver.findElements(By.linkText("Images")).get(0);
+		WebElement imagesLink = driver.findElements(By.linkText("Images")).get(0);
 		imagesLink.click();
 		
-		WebElement imageElement = driver.findElements(By.cssSelector("a")).get(0);
+	/*	WebElement imageElement = driver.findElements(By.cssSelector("a")).get(0);
 		WebElement imageLink = imageElement.findElements(By.tagName("img")).get(0);
 		imageLink.click();
-*/	
+	*/
 	}
 }
